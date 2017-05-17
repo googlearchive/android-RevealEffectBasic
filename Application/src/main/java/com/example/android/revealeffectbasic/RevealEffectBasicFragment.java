62eb7beba694f194d71e6230522b6b5ca611779c
google-automerger <automerger@google.com>
2017-05-17 13:34:37 -0700

    Auto-update

--- Application/src/main/java/com/example/android/revealeffectbasic/RevealEffectBasicFragment.java
+++ Application/src/main/java/com/example/android/revealeffectbasic/RevealEffectBasicFragment.java
@@ -15,18 +15,17 @@
  */
 package com.example.android.revealeffectbasic;
 
-import com.example.android.common.logger.Log;
-
 import android.animation.Animator;
-import android.animation.ValueAnimator;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
+
+import com.example.android.common.logger.Log;
 
 /**
  * This sample shows a view that is revealed when a button is clicked.
@@ -43,37 +42,34 @@ public class RevealEffectBasicFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        final View rootView = inflater.inflate(R.layout.reveal_effect_basic, container, false);
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.reveal_effect_basic, container, false);
+    }
 
-        View button = rootView.findViewById(R.id.button);
-
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        final View shape = view.findViewById(R.id.circle);
+        final View button = view.findViewById(R.id.button);
         // Set a listener to reveal the view when clicked.
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                View shape = rootView.findViewById(R.id.circle);
-
                 // Create a reveal {@link Animator} that starts clipping the view from
                 // the top left corner until the whole view is covered.
-                Animator animator = ViewAnimationUtils.createCircularReveal(
+                Animator circularReveal = ViewAnimationUtils.createCircularReveal(
                         shape,
                         0,
                         0,
                         0,
                         (float) Math.hypot(shape.getWidth(), shape.getHeight()));
-
-                // Set a natural ease-in/ease-out interpolator.
-                animator.setInterpolator(new AccelerateDecelerateInterpolator());
+                circularReveal.setInterpolator(new AccelerateDecelerateInterpolator());
 
                 // Finally start the animation
-                animator.start();
+                circularReveal.start();
 
                 Log.d(TAG, "Starting Reveal animation");
             }
         });
-
-        return rootView;
+        super.onViewCreated(view, savedInstanceState);
     }
-
 }
